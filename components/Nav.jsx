49225cecf77dd00933b965761de76595d0e4abbc
Klaus <edith@Klauss-MacBook-Air.local>
2024-09-03 23:45:07 +0300

    introduce session providers and mobile view functions to make mobile view nav bar responsive

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,18 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
     const isUserLoggedIn = true;
+    
+    const [providers, setProviders ] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+          const res = await getProviders();
+          setProviders(res);
+
+          setProviders();
+        })();
+      }, []);
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
         <Link href="/" className='flex gap-2
@@ -43,8 +55,80 @@ const Nav = () => {
                 </div>
             ): (
                 <>
-                
+                {providers &&
+                    Object.values(providers).map((provider) => (
+                        <button
+                        type='button'
+                        key={provider.name}
+                        onClick={() => {
+                            signIn(provider.id);
+                        }}
+                        className='black_btn'
+                        >
+                        Sign in
+                        </button>
+                    ))}
                 </>
+            )}
+        </div>
+        {/* Mobile Navigation */}
+        <div className='sm:hidden flex relative'>
+            {isUserLoggedIn ? (
+            <div className='flex'>
+                <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+                onClick={() => setToggleDropdown(!toggleDropdown)}
+                />
+
+                {toggleDropdown && (
+                <div className='dropdown'>
+                    <Link
+                    href='/profile'
+                    className='dropdown_link'
+                    onClick={() => setToggleDropdown(false)}
+                    >
+                    My Profile
+                    </Link>
+                    <Link
+                    href='/create-prompt'
+                    className='dropdown_link'
+                    onClick={() => setToggleDropdown(false)}
+                    >
+                    Create Prompt
+                    </Link>
+                    <button
+                    type='button'
+                    onClick={() => {
+                        setToggleDropdown(false);
+                        signOut();
+                    }}
+                    className='mt-5 w-full black_btn'
+                    >
+                    Sign Out
+                    </button>
+                </div>
+                )}
+            </div>
+            ) : (
+            <>
+                {providers &&
+                Object.values(providers).map((provider) => (
+                    <button
+                    type='button'
+                    key={provider.name}
+                    onClick={() => {
+                        signIn(provider.id);
+                    }}
+                    className='black_btn'
+                    >
+                    Sign in
+                    </button>
+                ))}
+            </>
             )}
         </div>
     </nav>
